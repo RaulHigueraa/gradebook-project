13b8791c16d39f0e0b2c6aad7d1d51e02fa25362
melanie <mtorres336@toromail.csudh.edu>
2026-05-03 23:44:17 -0700

    Update course.c
    worked on findstudent :
    -searching for course in array 
    -searching for student in 
    -properly displaying students information if found

--- src/course.c
+++ src/course.c
@@ -25,39 +25,64 @@ char *getCourseName(course *c) {
 }
 void findstudent(){
         char subject[50];
+        //found will keep track on whether course is found 
+        //found = 0 means course was not found and if found = 1 course was found in array
+        int found = 0;
+        // key will hold the id number the user inputs
         int key;
         //ask for course name to search for student in
         printf("Enter the course you want to look for a student in: ");
-        scanf("%[^\n]s",&subject);
+        scanf("%%49\n",&subject);
         
         //first check if course exsits 
         // if it does ask for id, if it doesn't return to main menu
-      for (int i=0; i<//course array len; i++)
+        //strcmp will compare two strings character by character, 0 means the strings are equal
+      for (int i=0; i<courseCount; i++)
       {
-        if(subjet == course[i].course_name)
-            print("Course found...)"    
-        else 
-            printf("Course not found... returning...");
-            return;
+        if(strcmp (subject, course[i].course_name)==0)
+            print("Course found...)" 
+            found = 1;
       }
+      if(!found)
+      {
+          printd("Course was not found...\n")
+          return;
+      }
+    
         //asking user for students id(key)
         // used id for key incase their are students whith same name or name gets missspelled
-        int key;
-        printf("Enter the students id number: ");
-        scanf("%10d",&key);
-        //access course and look for student in course 
-        for (int i=0; i<//student array len;i++);
+        printf("Enter the student's ID number: ");
+        scanf("%d",&key);
+        
+        //access course and look for student in course
+        gradebook *gb = &coursescourseIndex.gradebook;
+        for (int i=0; i<gb->studnetCount; i++);
           {
-                if (student[i].id_num == key) 
+                if (gb->students[i].id_num == key) 
                 {
                         printf("Student found.\n");
-                        //display students current info
+                        //display students current info such as 
+                        //name, id, hw scores, project scores, test scores
                         printf("-----------------Student Infromation------------------------\n");
-                        printf("Student Name: %s\n",student[i].name);
-                        print("ID: %d\n",student[i].id_number);
-                        printf("Homework grade: %f\n", student[i].hw_scores);
-                        print("Exam grade: %f\n", student[i].exam_scores);
-                        printf("Project grade: %f\n",student[i].project_scores);
+                        printf("Student Name: %s\n",gb->students[i].name);
+                        print("ID: %d\n", gb->students[i].id_number);
+                        
+                        //use for loop to display scores for each hw, exam, and projects
+                        printf("Homework scores: \n");
+                        for(int a=0; a<gb->students[i].homeworkCount; a++)
+                        {
+                            printf("%f\n", gb->student[i].hw_scores[a] );
+                        }
+                        
+                        printf("Exam Scores: \n"); 
+                        for(int b =0; b<gb->studnets[i].examCount; b++)
+                        {
+                            print("%f\n", gb->students[i].exam_scores[b]);
+                        }
+                        printf("Project scores: \n");
+                        for(int x = 0; x<gb->students[i].project_scores; x++){
+                        printf("%f\n",gb->students[i].project_scores[x]);}
+                    
                         return;
                     
                 }
